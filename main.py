25bb1d219fb823b2f77fdc0d891bbb1e89422fe0
Julia-Makes-Games <juliamakesgames@gmail.com>
2023-03-19 17:12:27 -0500

    Got a working simulated parliament, TODO: Keyboard functionality, graphical representation of votes, and testing in actual play game

---
+++ main.py
@@ -0,0 +1,139 @@
+import json
+import random
+import os
+from pop import pop
+from definitions import *
+
+# MODIFIABLE VARIABLES
+# FACTION POPULATION
+totalPop = 0
+militarists = 0
+xenPhobes = 0
+egalitarians = 0
+materialists = 0
+pacifists = 0
+xenPhiles = 0
+authoritarians = 0
+spiritualists = 0
+
+# PROPOSAL SETTINGS
+operation = RUN_ELECTION # RUN_ELECTION or SIM_PARLIAMENT
+ethicOfProposal = PACIFIST
+twoThirdsREQ = False
+unanimousREQ = False
+crisisSolution = False
+underThreat = False
+
+parliament: list = []
+
+
+def createParliament(numMil, numXenPho, numEgal, numMat, numPac, numXenPhi, numAuth, numSpir):
+    global parliament
+    parliament = []
+    for i in range(totalPop):
+        if numMil != 0:
+            tempPop = pop(MILITARIST)
+            numMil = numMil - 1
+        elif numXenPho != 0:
+            tempPop = pop(XENOPHOBE)
+            numXenPho = numXenPho - 1
+        elif numEgal != 0:
+            tempPop = pop(EGALITARIAN)
+            numEgal = numEgal - 1
+        elif numMat != 0:
+            tempPop = pop(MATERIALIST)
+            numMat = numMat - 1
+        elif numPac != 0:
+            tempPop = pop(PACIFIST)
+            numPac = numPac - 1
+        elif numXenPhi != 0:
+            tempPop = pop(XENOPHILE)
+            numXenPhi = numXenPhi - 1
+        elif numAuth != 0:
+            tempPop = pop(AUTHORITARIAN)
+            numAuth = numAuth - 1
+        elif numSpir != 0:
+            tempPop = pop(SPIRITUALIST)
+            numSpir = numSpir - 1
+        else:
+            tempPop = pop(random.randint(0,8)) # Random ethics if population is larger than factions
+        parliament.append(tempPop)
+    storeParliament()
+
+def storeParliament():
+    output = []
+    for i in parliament:
+        temp = {
+            "militarist": i.ethicsArr[MILITARIST],
+            "xenophobe": i.ethicsArr[XENOPHOBE],
+            "egalitarian": i.ethicsArr[EGALITARIAN],
+            "materialist": i.ethicsArr[MATERIALIST],
+            "pacifist": i.ethicsArr[PACIFIST],
+            "xenophile": i.ethicsArr[XENOPHILE],
+            "authoritarian": i.ethicsArr[AUTHORITARIAN],
+            "spiritualist": i.ethicsArr[SPIRITUALIST],
+            "volitility": i.volitility
+        }
+        output.append(temp)
+    with open("parliament.json", 'w') as outFile:
+        json.dump(output,outFile, indent=4)
+
+def readParliament():
+    with open("parliament.json", 'r') as inFile:
+        data = json.load(inFile)
+        for i in data:
+            tempEthics = []
+            tempEthics.append(i.get("militarist"))
+            tempEthics.append(i.get("xenophobe"))
+            tempEthics.append(i.get("egalitarian"))
+            tempEthics.append(i.get("materialist"))
+            tempEthics.append(i.get("pacifist"))
+            tempEthics.append(i.get("xenophile"))
+            tempEthics.append(i.get("authoritarian"))
+            tempEthics.append(i.get("spiritualist"))
+            parliament.append(pop(tempEthics[MILITARIST],tempEthics[XENOPHOBE],tempEthics[EGALITARIAN],
+                                  tempEthics[MATERIALIST],tempEthics[PACIFIST],tempEthics[XENOPHILE],
+                                  tempEthics[AUTHORITARIAN],tempEthics[SPIRITUALIST],i.get("volitility")))
+
+
+def runVote():
+    inFavor = 0
+    against = 0
+    for i in parliament:
+        if i.vote(ethicOfProposal):
+            inFavor+=1
+        else:
+            against+=1
+    if unanimousREQ:
+        if against != 0:
+            print("VOTE FAILED")
+            return False
+        else:
+            print("VOTE PASSED")
+            return True
+    elif twoThirdsREQ:
+        if inFavor >= int(2 * (totalPop) / 3):
+            print("VOTE PASSED")
+            return True
+        else:
+            print("VOTE FAILED")
+            return False
+    else:
+        if inFavor > against:
+            print("VOTE PASSED")
+            return True
+        elif inFavor == against:
+            print("STALEMATE, ANOTHER VOTE REQUIRED")
+            return runVote()
+        else:
+            print("VOTE FAILED")
+            return False
+
+if operation == RUN_ELECTION:
+    createParliament(militarists, xenPhobes, egalitarians, materialists, pacifists, xenPhiles, authoritarians,spiritualists)
+    storeParliament()
+elif operation == SIM_PARLIAMENT:
+    readParliament()
+    runVote()
+else:
+    print("INVALID OPERATION")
---
+++ pop.py
@@ -0,0 +1,73 @@
+import random
+from definitions import *
+from multipledispatch import dispatch
+
+class pop:
+    @dispatch(int)
+    def __init__(self, ethic: int):
+        self.__volitility = random.randint(0,20)
+        self.__ethicsArr = [0 for i in range(8)]
+        self.ethicsGen(ethic)
+
+    @dispatch(int, int, int, int, int, int, int, int, int)
+    def __init__(self, mil, xepho, egal, mat, pac, xephi, auth, spir, vol):
+        self.__volitility = vol
+        self.__ethicsArr = [0 for i in range(8)]
+        self.__ethicsArr[MILITARIST] = mil
+        self.__ethicsArr[XENOPHOBE] = xepho
+        self.__ethicsArr[EGALITARIAN] = egal
+        self.__ethicsArr[MATERIALIST] = mat
+        self.__ethicsArr[PACIFIST] = pac
+        self.__ethicsArr[XENOPHILE] = xephi
+        self.__ethicsArr[AUTHORITARIAN] = auth
+        self.__ethicsArr[SPIRITUALIST] = spir
+
+
+    @property
+    def ethicsArr(self):
+        return self.__ethicsArr
+
+    @property
+    def volitility(self):
+        return self.__volitility
+
+    def vote(self, ethic):
+        voteNum = random.randint(0,100)
+        print(voteNum)
+        if voteNum <= self.__ethicsArr[ethic]:
+            return True
+
+    def ethicsGen(self, popEthic):
+        base = 5
+        for i in range(8): # randomly assign odds of voting in favor, minimum of 5 - volitility, max 75 + volitility
+            self.__ethicsArr[i] = base + random.randint(0,70) + (random.randint(-1,1)*self.__volitility)
+        if popEthic == MILITARIST:
+            self.__ethicsArr[MILITARIST] = 100-self.__volitility
+            self.__ethicsArr[PACIFIST] = self.__ethicsArr[PACIFIST]-50
+        elif popEthic == XENOPHOBE:
+            self.__ethicsArr[XENOPHOBE] = 100 - self.__volitility
+            self.__ethicsArr[XENOPHILE] = self.__ethicsArr[XENOPHILE] - 50
+        elif popEthic == EGALITARIAN:
+            self.__ethicsArr[EGALITARIAN] = 100 - self.__volitility
+            self.__ethicsArr[AUTHORITARIAN] = self.__ethicsArr[AUTHORITARIAN] - 50
+        elif popEthic == MATERIALIST:
+            self.__ethicsArr[MATERIALIST] = 100 - self.__volitility
+            self.__ethicsArr[SPIRITUALIST] = self.__ethicsArr[SPIRITUALIST] - 50
+        elif popEthic == PACIFIST:
+            self.__ethicsArr[PACIFIST] = 100 - self.__volitility
+            self.__ethicsArr[MILITARIST] = self.__ethicsArr[MILITARIST] - 50
+        elif popEthic == XENOPHILE:
+            self.__ethicsArr[XENOPHILE] = 100 - self.__volitility
+            self.__ethicsArr[XENOPHOBE] = self.__ethicsArr[XENOPHOBE] - 50
+        elif popEthic == AUTHORITARIAN:
+            self.__ethicsArr[AUTHORITARIAN] = 100 - self.__volitility
+            self.__ethicsArr[EGALITARIAN] = self.__ethicsArr[EGALITARIAN] - 50
+        elif popEthic == SPIRITUALIST:
+            self.__ethicsArr[SPIRITUALIST] = 100 - self.__volitility
+            self.__ethicsArr[MATERIALIST] = self.__ethicsArr[MATERIALIST] - 50
+
+        for i in range(8): # checks for min and max
+            if self.__ethicsArr[i] < 0:
+                self.__ethicsArr[i] = 0
+            elif self.__ethicsArr[i] > 100:
+                self.__ethicsArr[i] = 100
